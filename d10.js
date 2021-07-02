876098758602e9395f679367313367737d5449e9
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-07-02 14:45:07 +0100

    Updated <100 to 101

--- d10.js
+++ d10.js
@@ -23,7 +23,7 @@ console.log(arrayTen);
 // also not logging in the console for some reason
 
 let numArray = function random(array) {
-    for  (let a=[], i=0;i<100;++i) a[i]=i;
+    for  (let a=[], i=0;i<101;++i) a[i]=i;
     let tmp, current, top = array.length;
     if (top) while(--top) {
         current = Math.floor(Math.random() * (top + 1));
