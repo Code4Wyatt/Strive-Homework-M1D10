d0a68fcd43598fa463d5c1b1cde382be734a5aa7
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-07-02 12:31:02 +0100

    Created array with 100 numbers

--- d10.js
+++ d10.js
@@ -17,3 +17,14 @@ console.log(obj);
 // need to revisit and find out why its not logging in the console
 
 const arrayTen = ["This", "Is", "An", "Array", "and", "in", "it", "is", "10", "Strings"];
+
+console.log(arrayTen);
+
+// also not logging in the console for some reason
+
+let numArray = new Array(100);
+
+console.log(numArray);
+
+// not logging in console
+
